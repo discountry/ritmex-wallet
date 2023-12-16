a272239a3f75cfe124d0980b78b063e279ae7ca0
discountry <feibilanceon@gmail.com>
2023-12-17 01:28:53 +0800

    add zrc20 index

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThirdwebProvider } from "@/components/ClientProvider";
-import {
-  metamaskWallet,
-  okxWallet,
-  coinbaseWallet,
-  walletConnect,
-} from "@thirdweb-dev/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,12 +19,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ThirdwebProvider
-          supportedWallets={[
-            metamaskWallet(),
-            okxWallet(),
-            coinbaseWallet(),
-            walletConnect(),
-          ]}
           activeChain="arbitrum"
           clientId={process.env.NEXT_PUBLIC_CLIENT_ID ?? ""}
         >
